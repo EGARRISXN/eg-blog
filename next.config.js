131ec157789a080a72da54242cecc006818bea3b
EGARRISXN <EGARRISXN@gmail.com>
2023-12-03 00:03:02 -0500

    edited my protected client page.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-const withMDX = require("@next/mdx")();
+const withMDX = require('@next/mdx')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
-    domains: ["github.com", "lh3.googleusercontent.com"],
+    domains: ['github.com', 'lh3.googleusercontent.com'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'avatars.githubusercontent.com',
+    //     port: '',
+    //     pathname: '/u/**',
+    //   },
+    // ],
   },
 };
 
--- src/app/protected/client/page.jsx
+++ src/app/protected/client/page.jsx
@@ -1,3 +1,4 @@
+'use client';
 //! You must use an AuthProvider for client components to useSession
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
