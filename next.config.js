42f42fbb2de7e7956db6ce5364ad9ebe86ef785d
EGARRISXN <EGARRISXN@gmail.com>
2023-12-08 16:59:33 -0500

    mdx stuff and things.

--- next.config.js
+++ next.config.js
@@ -4,38 +4,24 @@ const withMDX = require('@next/mdx')();
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'avatars.githubusercontent.com',
-      'pbs.twimg.com',
-      'cdn.discordapp.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'lh3.googleusercontent.com',
-    //     port: '',
-    //     pathname: '/a/**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'avatars.githubusercontent.com',
-    //     port: '',
-    //     pathname: '/u/**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'cdn.discordapp.com',
-    //     port: '',
-    //     pathname: '/avatars/**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'pbs.twimg.com',
-    //     port: '',
-    //     pathname: '/profile_images/**',
-    //   },
-    // ],
   },
 };
 
